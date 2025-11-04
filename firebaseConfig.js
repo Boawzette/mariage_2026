8964a5e209142f7612a08d8e9bca91d61d20ae6d
Boawzette <anais.pera@gmail.com>
2025-11-04 23:21:30 +0100

    Remove Firebase configuration and initialization

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,19 +1,1 @@
-// Import de Firebase
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth"; // <== important pour l’auth
 
-// Configuration de ton projet
-const firebaseConfig = {
-  apiKey: "AIzaSyDlj8Ke-C9jJGqiBSeDHx-oXZBUbV2d36o",
-  authDomain: "mariage-anais-louis.firebaseapp.com",
-  projectId: "mariage-anais-louis",
-  storageBucket: "mariage-anais-louis.firebasestorage.app",
-  messagingSenderId: "1084105955838",
-  appId: "1:1084105955838:web:b2d07943ecc19096a2c8af"
-};
-
-// Initialise Firebase
-const app = initializeApp(firebaseConfig);
-
-// Configure l’authentification
-export const auth = getAuth(app);
