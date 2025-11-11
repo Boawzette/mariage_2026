041cfc3730a26a22f221ce35aed0eb7b64558fe8
Boawzette <anais.pera@gmail.com>
2025-11-11 23:47:56 +0100

    Rename exported function for Next.js integration
    Refactor Firebase function export for Next.js app.

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,18 @@
 const functions = require("firebase-functions");
 const next = require("next");
 
+// Détecte si on est en dev ou prod
 const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev, conf: { distDir: ".next" } });
+
+// Configure Next.js pour utiliser le dossier de build `.next`
+const app = next({
+  dev,
+  conf: { distDir: ".next" },
+});
+
 const handle = app.getRequestHandler();
 
-exports.nextApp = functions.https.onRequest((req, res) => {
+// Fonction exportée pour Firebase
+exports.nextjsFunc = functions.https.onRequest((req, res) => {
   return app.prepare().then(() => handle(req, res));
 });
