c1873ec2588b00d15c0b5da378d251e1ffbd3453
Boawzette <anais.pera@gmail.com>
2025-11-11 23:30:09 +0100

    Configure Next.js for static export and strict mode
    Set output to 'export' for complete static export and enable React strict mode.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export', // <-- permet l'export statique complet
+  reactStrictMode: true,
+};
 
 export default nextConfig;
