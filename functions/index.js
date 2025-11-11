96434172baafa8db7f4e351836dd1225dbf80cc7
Boawzette <anais.pera@gmail.com>
2025-11-11 23:59:38 +0100

    Refactor Firebase function to export nextApp

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,13 @@
+
 const functions = require("firebase-functions");
 const next = require("next");
 
-// Détecte si on est en dev ou prod
-const dev = process.env.NODE_ENV !== "production";
 
-// Configure Next.js pour utiliser le dossier de build `.next`
-const app = next({
-  dev,
-  conf: { distDir: ".next" },
-});
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev, conf: { distDir: ".next" } });
 
 const handle = app.getRequestHandler();
 
-// Fonction exportée pour Firebase
-exports.nextjsFunc = functions.https.onRequest((req, res) => {
+exports.nextApp = functions.https.onRequest((req, res) => {
   return app.prepare().then(() => handle(req, res));
 });
